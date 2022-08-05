eb96b434276a8f120f66e44436a0c1e77ebd7479
Tanmoy <tanmoykhatua97@gmail.com>
2022-08-05 11:21:14 +0530

    create a react app and publish it to Github pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+// GET PROVIDER FROM THE REACT REDUX
+import { Provider } from "react-redux";
+// IMPORT STORE
+import store from "./redux/store";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+ReactDOM.render(
+  // SEDING STORE AS A PROPS
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
